96edb0a8b91c5204a831608e3be257932530c751
Gonzalo Nahuel <gonzalo.nahuel.dipierro@gmail.com>
2016-06-26 17:49:07 -0300

    Se verifica que se inicialice una Bomba con la vida y masa por default

---
+++ spec/Bomba_spec.rb
@@ -0,0 +1,53 @@
+require 'rspec'
+require 'spec_helper'
+require_relative '../model/Bomba'
+
+describe 'Bomba' do
+
+  it 'Se verifica que se inicialice una Bomba con la vida y masa por default' do
+
+    bomba = Bomba.new
+
+    #La vida y masa de la bomba deben ser 100 para cada una
+    expect(bomba.vida).to eq 100
+    expect(bomba.masa).to eq 100
+
+  end
+
+#  it 'Se verifica que se inicialice una Bomba con la vida indicada' do
+
+#    bomba = Bomba.new(300,200)
+
+    #La vida de la bomba debe ser 300
+#    expect(bomba.vida).to eq 300
+
+#  end
+
+#  it 'Se verifica que se inicialice una Bomba con la masa indicada' do
+
+#    bomba = Bomba.new(500,400)
+
+    #La masa de la bomba debe ser 400
+#    expect(bomba.masa).to eq 400
+
+#  end
+
+#  it 'Se verifica que se inicialice una Bomba con la vida en 0 (Cero) cuando se inserta un numero negativo para la vida' do
+
+#    bomba = Bomba.new(-100,100)
+
+    #La vida de la bomba debe ser 0
+#    expect(bomba.vida).to eq 0
+
+#  end
+
+#  it 'Se verifica que se inicialice una Bomba con la masa en 0 (Cero) cuando se inserta un numero negativo para la masa' do
+
+#    bomba = Bomba.new(100,-100)
+
+    #La masa de la bomba debe ser 0
+#    expect(bomba.masa).to eq 0
+
+#  end
+
+end
