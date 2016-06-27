e1c82a1943930e1ce5f8801468ad245a69f39dbe
Gonzalo Nahuel <gonzalo.nahuel.dipierro@gmail.com>
2016-06-27 01:05:11 -0300

    Se verifica que un Objeto Espacial este vivo cuando se inicializa con una vida y masa determinada

--- spec/ObjetoEspacial_spec.rb
+++ spec/ObjetoEspacial_spec.rb
@@ -9,4 +9,9 @@ describe 'ObjetoEspacial' do
     expect(objeto_espacial.esta_vivo).to eq true
   end
 
+  it 'Se verifica que un Objeto Espacial este vivo cuando se inicializa con una vida y masa determinada' do
+    objeto_espacial = ObjetoEspacial.new(100,100)
+    expect(objeto_espacial.esta_vivo).to eq true
+  end
+
 end
