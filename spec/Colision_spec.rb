c9517378e17cba5c251187625a76649c4ba01840
Gonzalo Nahuel <gonzalo.nahuel.dipierro@gmail.com>
2016-06-27 01:53:45 -0300

    Se verifica que se inicialice una Colision con objetos espaciales vivos

--- spec/Colision_spec.rb
+++ spec/Colision_spec.rb
@@ -1,6 +1,7 @@
 require 'rspec'
 require 'spec_helper'
 require_relative '../model/Colision'
+require_relative '../model/ObjetoMuertoException'
 
 describe 'Colision' do
 
@@ -10,5 +11,26 @@ describe 'Colision' do
 
     colision = Colision.new(primer_objeto_espacial, segundo_objeto_espacial)
   end
+
+#  it 'Se verifica que no se inicialice una Colision con el objeto colisionador muerto' do
+#    primer_objeto_espacial = ObjetoEspacial.new(0,0)
+#    segundo_objeto_espacial = ObjetoEspacial.new
+
+#    expect {colision = Colision.new(primer_objeto_espacial, segundo_objeto_espacial)}.to raise_exception ObjetoMuertoException
+#  end
+
+#  it 'Se verifica que no se inicialice una Colision con el objeto colisionado muerto' do
+#    primer_objeto_espacial = ObjetoEspacial.new
+#    segundo_objeto_espacial = ObjetoEspacial.new(0,0)
+
+#    expect {colision = Colision.new(primer_objeto_espacial, segundo_objeto_espacial)}.to raise_exception ObjetoMuertoException
+#  end
+
+#  it 'Se verifica que no se inicialice una Colision con el objeto colisionador y colisionado muertos' do
+#    primer_objeto_espacial = ObjetoEspacial.new(0,0)
+#    segundo_objeto_espacial = ObjetoEspacial.new(0,0)
+
+#    expect {colision = Colision.new(primer_objeto_espacial, segundo_objeto_espacial)}.to raise_exception ObjetoMuertoException
+#  end
 
 end
