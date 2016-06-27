4ab4cf49c12d0aed18d2a20dc10e8381dc4e1e24
Gonzalo Nahuel <gonzalo.nahuel.dipierro@gmail.com>
2016-06-27 08:00:12 -0300

    Agregado un comentario en la clase Efecto

--- model/Efecto.rb
+++ model/Efecto.rb
@@ -1,8 +1,12 @@
 class Efecto
 
+  #El nombre dato_requerido considero que es poco claro, pero realmente no pude encontrar otro que generalice mejor.
+  #Cada efecto recibe un solo dato que puede ser desde un porcentaje (Negativo o positivo) hasta un numero entero.
+  #Como los tipos de datos pueden variar, me cuesta encontrarle un nombre a la variable que generalice a todos ellos.
   def initialize(*dato_requerido)
     @dato_requerido = dato_requerido[0]
   end
+  #
 
   def aplicar(*objeto_espacial) end
 
