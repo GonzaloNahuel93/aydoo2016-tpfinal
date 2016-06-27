536b394c1f027d52a55aaf0c3a0dd1e506fd8de4
Gonzalo Nahuel <gonzalo.nahuel.dipierro@gmail.com>
2016-06-27 08:03:10 -0300

    Agregado comentario en la clase EfectoConstructivo

--- model/EfectoConstructivo.rb
+++ model/EfectoConstructivo.rb
@@ -2,6 +2,7 @@ require_relative '../model/Efecto'
 
 class EfectoConstructivo < Efecto
 
+  #El Efecto Constructivo incrementa las unidades de vida de un objeto en una cantidad igual a la vida de otro objeto.
   def aplicar(*objeto_espacial)
 
     primer_objeto_espacial = objeto_espacial[0]
