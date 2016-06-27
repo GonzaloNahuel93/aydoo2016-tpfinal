77c4fe465756e0834c19676b676846ba4066b12e
Gonzalo Nahuel <gonzalo.nahuel.dipierro@gmail.com>
2016-06-27 01:55:55 -0300

    Se verifica que no se inicialice una Colision con el objeto colisionado muerto

--- spec/Colision_spec.rb
+++ spec/Colision_spec.rb
@@ -19,12 +19,12 @@ describe 'Colision' do
     expect {colision = Colision.new(primer_objeto_espacial, segundo_objeto_espacial)}.to raise_exception ObjetoMuertoException
   end
 
-#  it 'Se verifica que no se inicialice una Colision con el objeto colisionado muerto' do
-#    primer_objeto_espacial = ObjetoEspacial.new
-#    segundo_objeto_espacial = ObjetoEspacial.new(0,0)
+  it 'Se verifica que no se inicialice una Colision con el objeto colisionado muerto' do
+    primer_objeto_espacial = ObjetoEspacial.new
+    segundo_objeto_espacial = ObjetoEspacial.new(0,0)
 
-#    expect {colision = Colision.new(primer_objeto_espacial, segundo_objeto_espacial)}.to raise_exception ObjetoMuertoException
-#  end
+    expect {colision = Colision.new(primer_objeto_espacial, segundo_objeto_espacial)}.to raise_exception ObjetoMuertoException
+  end
 
 #  it 'Se verifica que no se inicialice una Colision con el objeto colisionador y colisionado muertos' do
 #    primer_objeto_espacial = ObjetoEspacial.new(0,0)
